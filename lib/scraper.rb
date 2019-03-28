e4604322b3ffa69dc9bebe60f4898dcd1c581e33
Alberto Morales <cmtmorales24@gmail.com>
2019-03-28 23:41:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ require_relative './course.rb'
   class Scraper
   
   def get_page 
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
     #doc.css(".post").each do |post|
      # course = Course.new
@@ -19,6 +19,9 @@ require_relative './course.rb'
     
     #end 
   end 
+  
+    def get_courses 
+      
   
     def print_courses
     self.make_courses
